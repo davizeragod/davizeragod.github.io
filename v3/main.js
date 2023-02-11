1ecabd8f91e7d94e258e96b3f3dc5f95c7579875
davizeragod <44541350+davizeragod@users.noreply.github.com>
2023-02-11 00:24:28 -0300

    Update main.js

--- v3/main.js
+++ v3/main.js
@@ -59,10 +59,6 @@ function fazGet(url) {
   let request = new XMLHttpRequest();
   request.open("GET", url, false);
   request.send();
-  while (request.status != 200){
-    request.open("GET", url, false);
-    request.send();
-  }
   return request.responseText;
 }
 
