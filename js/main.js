1cbc77822467b73bb383947ddb001e3333da5023
davizeragod <44541350+davizeragod@users.noreply.github.com>
2024-09-04 23:41:34 -0300

    page nova muito foda!! !!! ! ! !!

--- js/main.js
+++ js/main.js
@@ -1,105 +1,154 @@
-const chkboxicon = document.getElementById("fundoimg");
-const chkboxbg = document.getElementById("iconlado");
-const urlicon = document.getElementById("urlicon");
-const urlbg = document.getElementById("urlbg");
-const botao = document.querySelector("button");
-const cortexto = document.getElementById("txt");
-const corfundo = document.getElementById("fundo");
-const iframe = document.getElementById("framefoda");
-const chfundoimg = document.getElementById("fundoimg");
-const chfundoico = document.getElementById("iconlado");
-const iframetxt = document.getElementById("lblframe");
-const reg = document.getElementById("regiao");
-const idu = document.getElementById("lname");
-const nmu = document.getElementById("fname");
-const apikey = document.getElementById("apikey");
-const trchck = document.getElementById("transparentcheck");
-const disc = document.getElementById("bannerpic");
-const divdisc = document.getElementById("lblbanner");
-let trachck = {};
+const copyText = document.getElementById("linkUrl"),
+revealEffect = document.querySelectorAll('.reveal'),
+frame = document.querySelector("iframe"),
+copyLink = document.querySelector("#copyLink");
 
-chfundoico.addEventListener("change", (event) => {
-  if (event.currentTarget.checked) {
-    document.getElementById("urlicon").disabled = false;
-  } else {
-    document.getElementById("urlicon").disabled = true;
-  }
+(() => {
+    setTimeout(() => {
+        document.querySelector("#pfp").classList.add("float-effect");
+    }, 1000);
+
+    ["bu", "iu", "color", "fontcolor"].forEach((e) => {
+        let locVal = localStorage.getItem(e);
+        console.log("loc", e, locVal);
+        if (locVal) {
+            document.querySelector('input[name="' + e + '"]').value =
+                localStorage.getItem(e);
+        }
+    });
+
+    const locAlpha = localStorage.getItem("alpha");
+    console.log(locAlpha);
+    document.querySelector('input[name="alpha"]').checked =
+        locAlpha && locAlpha != "false";
+})();
+
+document.querySelectorAll("#closeDropdown, #toggleMenu").forEach((e) =>
+    e.addEventListener("click", () => {
+        const menu = document.querySelector("#menu");
+        if (menu.classList.contains("hidden")) {
+            menu.classList.remove("hidden");
+            menu.classList.add("flex", "reveal-r", "active");
+            return;
+        }
+        menu.classList.add("hidden");
+        menu.classList.remove("flex", "reveal-r", "active");
+    })
+);
+
+copyLink.addEventListener("click", () => {
+    if (!copyText.value) return;
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
 });
 
-chfundoimg.addEventListener("change", (event) => {
-  if (event.currentTarget.checked) {
-    document.getElementById("urlbg").disabled = false;
-  } else {
-    document.getElementById("urlbg").disabled = true;
-  }
+copyText.addEventListener("focus", (e) => {
+    e.target.select();
+    e.target.setSelectionRange(0, 99999);
 });
 
-reg.addEventListener("change", function handlechange(event) {
-  if (event.target.value === 'AP') {
-    disc.style.display = "inline-block";
-    divdisc.style.display = "flex";
-  } else {
-    disc.style.display = "none";
-    divdisc.style.display = "none";
-  }
-});
+const fields = document.querySelectorAll("input[name], select[name"),
+    validationRule = {
+        default: (v, k) => !k || !!v?.length,
+        color: (v, k) => {
+            localStorage.setItem(k, v);
+            return [true, v.replace("#", "")];
+        },
+        fontcolor: (v, k) => validationRule.color(v, k),
+        alpha: (v, k) => {
+            localStorage.setItem(k, v);
+            return [true, v ? "ss" : "nn"];
+        },
 
-botao.onclick = function () {
-  var regiao = reg.value;
-  let idusu = idu.value;
-  let nmusu = nmu.value;
-  let bgurl = urlbg.value;
-  let chaveapi = apikey.value;
-  let iurl = urlicon.value;
-  TxtUrlBg = {};
-  TxtUrlIco = {};
+        bu: (v, k) => {
+            localStorage.setItem(k, v || "");
+            return [true, encodeURIComponent(v || "")];
+        },
+        iu: (v, k) => validationRule.bu(v, k),
+    };
 
-  if (urlbg.value.length != 0) {
-    localStorage.setItem(TxtUrlBg, bgurl);
-  }
-  if (urlicon.value.length != 0) {
-    localStorage.setItem(TxtUrlIco, iurl);
-  }
+let timeoutQuery;
+fields.forEach((e) => {
+    e.addEventListener("keyup", changes)
+    e.addEventListener("change", changes)
 
-  const link = document.querySelector("textarea");
-  let fontcolor = cortexto.value.replace("#", "");
-  let color = corfundo.value.replace("#", "");
-  if (idu.value.length == 0) {
-    alert("Please fill all fields");
-    return;
-  }
-  if (nmu.value.length == 0) {
-    alert("Please fill all fields");
-    return;
-  }
+}
+);
 
-  if (trchck.checked) {
-    trachck = "ss";
-  } else {
-    trachck = "nn";
-  }
-  link.value =
-    window.location.origin +
-    "/v2/main.html?regiao=" +
-    regiao +
-    "&nmusuario=" +
-    nmusu +
-    "&idusuario=" +
-    idusu +
-    "&fontcolor=" +
-    fontcolor +
-    "&color=" +
-    color +
-    "&alpha=" +
-    trachck +
-    "&iu=" +
-    encodeURIComponent(iurl) +
-    "&bu=" +
-    encodeURIComponent(bgurl) +
-    "&apikey=" +
-    chaveapi;
+function changes() {
+    if (!isValid()) {
+        copyText.value = "";
+        copyLink.classList.add("hidden");
+        frame.classList.add("hidden");
+        return;
+    }
+    const farea = document.getElementById("frame-area");
+    farea.classList.add("loading");
+    frame.classList.add("hidden");
+
+    if (timeoutQuery) clearTimeout(timeoutQuery);
+
+    timeoutQuery = setTimeout(() => {
+        let requestParams = {};
+        farea.classList.remove("loading");
+
+        if (isValid(requestParams)) {
+            console.log("requestParams", requestParams);
+            let params = "";
+            for (let x in requestParams) {
+                params += `${x}=${requestParams[x]}&`;
+            }
+            params = params.slice(0, -1);
+            console.log("params", params);
+            copyLink.classList.remove("hidden");
+
+            frame.classList.remove("hidden");
+            frame.src = copyText.value =
+                window.location.origin +
+                //"https://davizeragod.github.io" + 
+                "/v2/main.html?" + params;
+            console.log("update");
+            return;
+        }
+    }, 2000);
+}
+
+function isValid(requestParams = {}) {
+    return [...fields].every((e) => {
+        let u = (validationRule[e.name] || validationRule.default)(
+            e.getAttribute("type") == "checkbox" ? e.checked : e.value,
+            e.name
+        );
+        console.log(e.name, u);
+        if (typeof u === "boolean") {
+            requestParams[e.name] = e.value;
+            return u;
+        }
+
+        const [bol, newVal] = u;
+
+        requestParams[e.name] = newVal;
+        return bol;
+    });
+}
 
 
-  iframe.src = link.value;
-  iframetxt.innerHTML = "Preview:";
-};
+ // Text Observer
+ const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		// Check if the target element is at least 15% visible
+		if (entry.intersectionRatio >= 0.2) {
+			entry.target.classList.add("active");
+		} else {
+			entry.target.classList.remove("active");
+		}
+	});
+}, { threshold: 0.2 });
+
+for (var i = 0; i < revealEffect.length; i++) {
+	observer.observe(revealEffect[i]);
+}
