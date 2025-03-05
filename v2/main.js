bd71e795b03c623199f5b8679830c206bc132a6a
davizeragod <44541350+davizeragod@users.noreply.github.com>
2025-03-05 17:20:43 -0300

    Update main.js

--- v2/main.js
+++ v2/main.js
@@ -89,7 +89,7 @@ function main() {
       idusuario
   );
   var jsonData = JSON.parse(dados);
-  let currentAct = 'e10a1';
+  let currentAct = 'e10a2';
   isunrankedatoatual = jsonData.data.by_season[currentAct].number_of_games;
   nodataseasonatual = jsonData.data.by_season[currentAct].error;
   retornostatus = jsonData.status;
